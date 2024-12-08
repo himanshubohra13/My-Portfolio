9872f907cda2e4be327d9922771bfcc158ea88be
himanshubohra13 <himanshuue195061ece@gmail.com>
2024-12-08 15:43:02 +0530

    publishing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/himanshubohra.github.io/' : '',
+  basePath: isProd ? '/himanshubohra.github.io' : '',
+  output: 'export'
+};
 
 export default nextConfig;
